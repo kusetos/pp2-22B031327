5eaa7cbefe64470cc93cd99df32ee529af992a59
kusetos <123006797+kusetos@users.noreply.github.com>
2023-01-24 21:31:18 +0600

    Create conditions.py

---
+++ conditions.py
@@ -0,0 +1,13 @@
+num1 = int(input())
+num2 = int(input())
+if num1 == num2:
+  print("equal")
+elif num1 <= num2:
+  print("num2 bigger")
+else:
+  print(num2 smaller)
+if num1 != num2: print("beket lox")
+print(1) if num1 > num2 else print(2)
+print("big")if num1 > 0 and num2 > 0 else print("ne big") if num1 > 0 and num2 > 0 else print("pensil")
+if num1 == num2:
+    pass
